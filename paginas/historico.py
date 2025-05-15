ec544bc9caa6c130e45bdbdfc62ad8cb6e7d0a00
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2025-05-15 17:12:16 -0300

    Update historico.py

--- paginas/historico.py
+++ paginas/historico.py
@@ -1,8 +1,12 @@
 # pages/historico.py
 
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 #st.set_page_config(page_title="Doenças da Soja", layout="wide")
 def display_content():
     st.title("🦠 Histórico de Pragas")
     st.markdown("Pragas identificadas...")
+    df = pd.DataFrame(np.random.randn(10, 5), columns=("Data e Hora", "Praga","Latitude","Longitude","Usuario"))    
+    st.dataframe(df)  # Same as st.write(df)
